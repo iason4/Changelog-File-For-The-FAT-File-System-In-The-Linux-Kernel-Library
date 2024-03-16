cee3a91c4359e63096847aefc926a8b4b26c44f1
iason4 <162754958+iason4@users.noreply.github.com>
2024-03-16 14:06:07 +0200

    Add files via upload

---
+++ lkl-source/filesc/hello.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main()
+{
+	char c[]="Hello !";
+	printf("%s\n",c);
+
+}
---
+++ lkl-source/filesc/sum.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	int x=5,y=9,sum;
+	sum=0;
+	printf("sum=%d\n",sum);
+
+}
